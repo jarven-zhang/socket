1217881777102235636f93ab05cd77e35f328166
jarven <hua_0602@163.com>
2019-01-16 20:18:43 +0800

    Update main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -21,7 +21,6 @@
 using namespace std;
 
 auto main(int argc, char* argv[]) -> int{
-#if 1
     google::InitGoogleLogging(argv[0]); //初始化 glog
 
     mkdir(GLOG_PATH, 0777);
@@ -38,40 +37,5 @@ auto main(int argc, char* argv[]) -> int{
         pServer->start();
     }
 
-//    google::ShutdownGoogleLogging();
-#else
-//    vector<string> va ;
-//    string client_address = "aaaa";
-
-//    va.push_back("11111");
-//    va.push_back("22222");
-//    va.push_back("33333");
-//    auto iter = find(va.begin(), va.end(), client_address);
-//    if(iter == va.end())
-//    {
-//        LOG(ERROR) << "!!!!Unknow clie.1`nt:" << client_address.c_str() << ", close the connector!";
-////        close(client_fd);
-////        continue;
-//    }
-
-
-    vector<int> vec;
-
-        vec.push_back(1);
-        vec.push_back(2);
-        vec.push_back(3);
-        vec.push_back(4);
-        vec.push_back(5);
-        vec.push_back(6);
-
-        vector<int>::iterator it = find(vec.begin(), vec.end(), 6);
-
-        if (it != vec.end())
-            cout<<*it<<endl;
-        else
-            cout<<"can not find"<<endl;
-
-
-#endif
     return 0;
 }
